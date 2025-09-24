0e7a0bc490072a8b7d3bbf64fc6856146c025dbc
YourGitHubUsername <ramuk13476@gmail.com>
2025-09-24 23:21:13 +0530

    basic-recursion

---
+++ Recursion/RecursionFun.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class RecursionFun {
+    
+    static void fun () {
+        System.out.println("1");
+fun();
+    }
+
+    public static void main(String[] args) {
+        fun();
+    }
+}

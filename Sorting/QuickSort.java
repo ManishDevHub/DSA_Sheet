8e4b7577e3f95ad825ce86334585f514ae77c095
YourGitHubUsername <ramuk13476@gmail.com>
2025-11-05 23:52:20 +0530

    quich-sort

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,53 @@
+public class QuickSort {
+
+    // Function to perform Quick Sort
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            // Find the partition index
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    // Partition method
+    public static int partition(int[] arr, int low, int high) {
+        // Choose the pivot (here we choose the last element)
+        int pivot = arr[high];
+        int i = low - 1; // index of smaller element
+
+        for (int j = low; j < high; j++) {
+            // If current element <= pivot
+            if (arr[j] <= pivot) {
+                i++;
+
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // Place pivot at the correct position
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1; // return the partition index
+    }
+
+    // Driver method to test the algorithm
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5};
+        int n = arr.length;
+
+        quickSort(arr, 0, n - 1);
+
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
